dff9b73e2cf17746da5c0310da667b6bac143857
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-11-08 20:24:35 -0700

    Pass file name in via command line argument
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/collaborative_filter.py
+++ src/collaborative_filter.py
@@ -4,45 +4,59 @@ from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
 from pyspark import SparkContext, SparkConf
 from src.parser import parse_line
 import os
+import argparse
 
 
-spark_home = os.environ['SPARK_HOME']
+def main():
 
-conf = SparkConf() \
-      .setAppName("Collaborative Filter") \
-      .set("spark.executor.memory", "5g")
+    parser = argparse.ArgumentParser(description='Create a collaborative filtering system for music ratings.')
+    parser.add_argument('datafile', type=str, nargs=1,
+                        help='absolute path to the file you wish use for testing/training')
 
-sc = SparkContext(conf=conf)
+    args = parser.parse_args()
+    path = args.datafile[0]
+    dataFile = 'file:///{}'.format(path)
+    print("dataFile = ", dataFile)
+
+    collaborative_filter(dataFile)
 
 
-dataFile = 'file:////Users/cody/Downloads/EvalDataYear1MSDWebsite/year1_test_triplets_hidden.txt'
+def collaborative_filter(dataFile):
 
-# Load and parse the data
-data = sc.textFile(dataFile.format(spark_home))
+    conf = SparkConf() \
+        .setAppName("Collaborative Filter") \
+        .set("spark.executor.memory", "5g")
+    sc = SparkContext(conf=conf)
+    # Load and parse the data
+    data = sc.textFile(dataFile)
+    ratings_map = data.map(parse_line)
+    count = ratings_map.count()
+    print("The count is: ", count)
+    ratings = ratings_map.map(lambda entry: Rating(entry['user']['hash'],
+                                                   entry['song']['hash'],
+                                                   entry['rating']))
+    # #ratings = data.map(lambda l: l.split()).map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
+    #
+    # # Build the recommendation model using Alternating Least Squares
+    rank = 10
+    numIterations = 10
+    model = ALS.train(ratings, rank, numIterations)
+    # # Evaluate the model on training data
+    # testdata = ratings.map(lambda p: (p[0], p[1]))
+    # predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+    # ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+    # MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+    # print("Mean Squared Error = " + str(MSE))
+    #
+    # # Save and load model
+    # path = os.path.dirname(os.path.realpath(__file__))
+    # path = 'file:///' + path + '/myModelPath'
+    # print(50*'*')
+    # print('The path is: ' + path)
+    # print(50*'*')
+    # model.save(sc, path)
+    # sameModel = MatrixFactorizationModel.load(sc, path)
 
-ratings_map = data.map(parse_line)
-count = ratings_map.count()
-print("The count is: ", count)
-ratings = ratings_map.map(lambda l: Rating(l['user']['hash'], l['song']['hash'], l['rating']))
-# #ratings = data.map(lambda l: l.split()).map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
-#
-# # Build the recommendation model using Alternating Least Squares
-rank = 10
-numIterations = 10
-model = ALS.train(ratings, rank, numIterations)
 
-# Evaluate the model on training data
-testdata = ratings.map(lambda p: (p[0], p[1]))
-predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
-ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
-MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
-print("Mean Squared Error = " + str(MSE))
-
-# Save and load model
-path = os.path.dirname(os.path.realpath(__file__))
-path = 'file:///' + path + '/myModelPath'
-print(50*'*')
-print('The path is: ' + path)
-print(50*'*')
-model.save(sc, path)
-sameModel = MatrixFactorizationModel.load(sc, path)
+if __name__ == '__main__':
+    main()
