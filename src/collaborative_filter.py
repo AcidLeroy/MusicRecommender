e8fd557ed0e20e9f38df2d940e948fe2da53242e
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-11-09 08:24:25 -0700

    Can successfully run on the sample dataset
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- src/collaborative_filter.py
+++ src/collaborative_filter.py
@@ -51,20 +51,20 @@ def collaborative_filter(dataFile):
     numIterations = int(10)
     model = ALS.train(ratings, rank, numIterations)
     # # Evaluate the model on training data
-    # testdata = ratings.map(lambda p: (p[0], p[1]))
-    # predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
-    # ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
-    # MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
-    # print("Mean Squared Error = " + str(MSE))
-    #
-    # # Save and load model
-    # path = os.path.dirname(os.path.realpath(__file__))
-    # path = 'file:///' + path + '/myModelPath'
-    # print(50*'*')
-    # print('The path is: ' + path)
-    # print(50*'*')
-    # model.save(sc, path)
-    # sameModel = MatrixFactorizationModel.load(sc, path)
+    testdata = ratings.map(lambda p: (p[0], p[1]))
+    predictions = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+    ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictions)
+    MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
+    print("Mean Squared Error = " + str(MSE))
+
+    # Save and load model
+    path = os.path.dirname(os.path.realpath(__file__))
+    path = 'file:///' + path + '/myModelPath'
+    print(50*'*')
+    print('The path is: ' + path)
+    print(50*'*')
+    model.save(sc, path)
+    sameModel = MatrixFactorizationModel.load(sc, path)
 
 
 if __name__ == '__main__':
--- src/parser.py
+++ src/parser.py
@@ -2,7 +2,7 @@ def parse_id(userid):
 
     # from some reason I have to use 7 'f's instead of 8 to get the
     # right number of bits or else spark will complain about not
-    # being able to cast from a long to an integer. 
+    # being able to cast from a long to an integer.
     return {'id': userid, 'hash': int(hash(userid) & 0xfffffff)}
 
 def parse_line(line):
