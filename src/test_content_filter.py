b82e6be90db23cc30c157754586cfbca6792decc
Cody W. Eilar <Cody.Eilar@Gmail.com>
2015-11-27 22:55:53 -0700

    Add simple (and terrible) script to demo the usage.
    - Hard coded paths are evil, but I left them in to make the code easier to read.
    
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

---
+++ src/test_content_filter.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from content_filter import append_cluster_id, get_new_ratings
+
+def main():
+    stored_file = '/Users/cody/extracted_song_features.h5'
+    training_file = '/Users/cody/Downloads/EvalDataYear1MSDWebsite/small_training_data.txt'
+    testing_file = '/Users/cody/Downloads/EvalDataYear1MSDWebsite/testing_data.txt'
+    song_features = pd.read_hdf(stored_file, '/features')
+    training_data = pd.read_csv(training_file, sep='\t', names=['user_id', 'song_id', 'rating'])
+    testing_data = pd.read_csv(testing_file, sep='\t', names=['user_id', 'song_id', 'rating'])
+    testing_data = append_cluster_id(testing_data, song_features[['song_id', 'cluster']])
+    training_data = append_cluster_id(training_data, song_features[['song_id', 'cluster']])
+    new_ratings = get_new_ratings(testing_data, training_data)
+    new_ratings = new_ratings.dropna()
+    new_ratings.to_csv('augmented_test_data.txt', sep='\t', header=False, index=False)
+
+if __name__ == '__main__':
+    main()
